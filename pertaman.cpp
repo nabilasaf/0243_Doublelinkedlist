e1224db7bb63cc11407f0141b207aa68e9eeb3f2
nabilasaf <nabilasafitri861@gmail.com>
2024-05-02 15:47:34 +0700

    menambahkan data nama ke dalam new node

--- pertaman.cpp
+++ pertaman.cpp
@@ -16,5 +16,7 @@ void addNode() {
     Node* newNode = new Node();  //step1 : create a new node
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs;      // assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;       // assign value to the data field of the new node
 };
 
