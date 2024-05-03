2bfe627a321b1c07131aed195601a83aaec89d74
nabilasaf <nabilasafitri861@gmail.com>
2024-05-03 23:16:18 +0700

    Make the previous field of the new node point to previous

--- pertaman.cpp
+++ pertaman.cpp
@@ -46,6 +46,7 @@ void addNode() {
         }
 
         newNode->next = current; //step 4: Make the next field of the new node point to current
+        newNode->prev = previous; //step 5: Make the previous field of the new node point to previous
     }
 };
 
